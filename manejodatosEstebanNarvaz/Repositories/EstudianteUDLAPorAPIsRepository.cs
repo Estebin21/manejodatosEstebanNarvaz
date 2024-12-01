c9047f2460b31bb1fded0d2774b456469df94ace
Esteban Alejandro Narváez Acurio <esteban.narvaez.acurio@udla.edu.ec>
2024-12-01 17:44:53 -0500

    cambio de api

---
+++ manejodatosEstebanNarvaz/Repositories/EstudianteUDLAPorAPIsRepository.cs
@@ -0,0 +1,47 @@
+﻿using manejodatosEstebanNarvaz.interfaces;
+using manejodatosEstebanNarvaz.modelos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejodatosEstebanNarvaz.Repositories
+{
+    public class EstudianteUDLAPorAPIsRepository : IEstudianteUDLARepository
+    {
+        public string _urlEndpoint = "https://www.freetestapi.com/api/v1/students";
+        public bool EliminarEstudianteUDLA(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IEstudianteUDLARepository.ActualizarEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        bool IEstudianteUDLARepository.CrearEstudianteUDLA(EstudianteUDLA estudiante)
+        {
+            throw new NotImplementedException();
+        }
+
+        EstudianteUDLA IEstudianteUDLARepository.DevuelveEstudianteUDLA(int id)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = httpClient.GetAsync(_urlEndpoint).Result;
+                var json_data = response.Content.ReadAsStringAsync().Result;
+
+                List<EstudianteAPI> estudianteAPIs = JsonConvert.DeserializeObject<List<EstudianteAPI>>(json_data);
+            }
+
+        }
+
+        IEnumerable<EstudianteUDLA> IEstudianteUDLARepository.DevuelveListadoEstudiante()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
